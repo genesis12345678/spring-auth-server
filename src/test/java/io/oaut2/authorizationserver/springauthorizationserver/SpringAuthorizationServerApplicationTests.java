7506a87e1c0749beeb4e36b1a449ff05fe00f119
장근영 <jky00914@naver.com>
2024-05-29 18:32:15 +0900

    settings

---
+++ src/test/java/io/oaut2/authorizationserver/springauthorizationserver/SpringAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.oaut2.authorizationserver.springauthorizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthorizationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
